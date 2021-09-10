2f2e6ac434b285f6ba335942620a7a27d7d7ac6e
Brett herrell <brett_herrell@yahoo.com>
2021-09-10 16:23:54 -0500

    buttons now add info to local storage. page will pull from local storage to display info after refresh

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,54 +1,73 @@
-// save info from buttons
+// save info from textboxes using buttons
 $("#nineBtn").click(function(){
     var nine = $("#nine").val();
-    console.log(nine);
+    localStorage.setItem('nine', nine);
 });
 
 $("#tenBtn").click(function(){
-    
-
     var ten = $("#ten").val();
     localStorage.setItem('ten', ten);
-    console.log(ten);
-    
-    console.log(localStorage)
 });
 
 $("#elevenBtn").click(function(){
     var eleven = $("#eleven").val();
-    console.log(eleven);
+    localStorage.setItem('eleven', eleven);
 });
 
 $("#twelveBtn").click(function(){
     var twelve = $("#twelve").val();
-    console.log(twelve);
+    localStorage.setItem('twelve', twelve);
 });
 
 $("#oneBtn").click(function(){
     var one = $("#one").val();
-    console.log(one);
+    localStorage.setItem('one', one);
 });
 
 $("#twoBtn").click(function(){
     var two = $("#two").val();
-    console.log(two);
+    localStorage.setItem('two', two);
 });
 
 $("#threeBtn").click(function(){
     var three = $("#three").val();
-    console.log(three);
+    localStorage.setItem('three', three);
 });
 
 $("#fourBtn").click(function(){
     var four = $("#four").val();
-    console.log(four);
+    localStorage.setItem('four', four);
 });
 
 $("#fiveBtn").click(function(){
     var five = $("#five").val();
-    console.log(five);
+    localStorage.setItem('five', five);
 });
 
 // load info from textboxes
-var loadTen = localStorage.getItem('ten')
-$('#ten').val(loadTen) 
+var loadNine = localStorage.getItem('nine');
+$('#nine').val(loadNine);
+
+var loadTen = localStorage.getItem('ten');
+$('#ten').val(loadTen);
+
+var loadEleven = localStorage.getItem('eleven');
+$('#eleven').val(loadEleven);
+
+var loadTwelve = localStorage.getItem('twelve');
+$('#twelve').val(loadTwelve);
+
+var loadOne = localStorage.getItem('one');
+$('#one').val(loadOne);
+
+var loadTwo = localStorage.getItem('two');
+$('#two').val(loadTwo);
+
+var loadThree = localStorage.getItem('three');
+$('#three').val(loadThree);
+
+var loadFour = localStorage.getItem('four');
+$('#four').val(loadFour);
+
+var loadFive = localStorage.getItem('five');
+$('#five').val(loadFive);
